5a84cff9ed89af2d5fd9c675e7b16d44aa36d11b
rudrawqw12 <jawadhossainrudra51@gmail.com>
2023-03-01 14:54:30 +0600

    ES6 RECAP

---
+++ one.js
@@ -0,0 +1,31 @@
+let data =
+[
+{
+"pHeroCourses": {
+"course-x": "web development"
+}
+},
+{
+"pHeroCourses": {
+"course-y": "phitron"
+}
+},
+{
+"pHeroCourses": {
+"course-z": "acc"
+}
+},
+{
+"pHeroCourses": {
+"course-xyz": "level-2"
+},
+"locationField": {
+"en-US": {
+"lat": 19.28563,
+"lon": 72.8691
+}
+}
+}
+]
+
+console.log(data);
